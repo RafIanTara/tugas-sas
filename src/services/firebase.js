3a49675551ae85d2c02201d558d4494e3c146c93
unknown <muhammad.rafa163418@smk.belajar.id>
2025-11-27 11:47:38 +0700

    alhamdulilah error hilang wak

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,14 +1,9 @@
 import { initializeApp } from "firebase/app";
-<<<<<<< HEAD
 // PENTING: Pakai initializeFirestore biar bisa setting long polling
-=======
-// PENTING: Pakai initializeFirestore, bukan getFirestore biasa
->>>>>>> 0f8f8bfa1daf2a39386283c1a129574157099473
 import { initializeFirestore } from "firebase/firestore"; 
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-<<<<<<< HEAD
   // --- JANGAN UBAH API KEY INI (PAKE PUNYA KAMU) ---
   apiKey: "AIzaSyDFJUxguohW_tqPYDhDNeeOFMe9yo2yvUw", 
   authDomain: "web-kelas-sas.firebaseapp.com", 
@@ -17,29 +12,14 @@ const firebaseConfig = {
   messagingSenderId: "313755533054", 
   appId: "1:313755533054:web:28346a4ce3ab5002836faf", 
   measurementId: "G-Y81VXDEPP9" 
-=======
-  // ... KODE API KEY KAMU YANG LAMA JANGAN DIHAPUS ...
-  apiKey: "AIzaSy...", 
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
->>>>>>> 0f8f8bfa1daf2a39386283c1a129574157099473
 };
 
 const app = initializeApp(firebaseConfig);
 
-<<<<<<< HEAD
 // --- OBAT KUAT KONEKSI ---
 // experimentalForceLongPolling: true -> Ini maksa lewat jalur HTTP biasa (Anti Blokir)
 const db = initializeFirestore(app, {
     experimentalForceLongPolling: true, 
-=======
-// --- BAGIAN INI OBATNYA WAK ---
-const db = initializeFirestore(app, {
-    experimentalForceLongPolling: true, // <--- INI WAJIB ADA
->>>>>>> 0f8f8bfa1daf2a39386283c1a129574157099473
 });
 
 const storage = getStorage(app);
